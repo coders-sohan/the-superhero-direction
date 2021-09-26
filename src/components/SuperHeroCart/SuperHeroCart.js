d3cd774f3d6ca81aca8560a5371a1ca4d32f2937
coders-sohan <coders.sohan.0216@gmail.com>
2021-09-26 12:37:17 +0600

    design updated

--- src/components/SuperHeroCart/SuperHeroCart.js
+++ src/components/SuperHeroCart/SuperHeroCart.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./SuperHeroCart.css";
 
 const SuperHeroCart = props => {
 	const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
 	const facebookIcon = <FontAwesomeIcon icon={faFacebook} />;
 	const twitterIcon = <FontAwesomeIcon icon={faTwitter} />;
+	const instagramIcon = <FontAwesomeIcon icon={faInstagram} />;
 	const { name, role, age, country, salary, img } = props.superHero;
 	return (
 		<div className="col">
@@ -37,6 +38,7 @@ const SuperHeroCart = props => {
 					<div className="py-2 mt-3 social-bg bg-secondary rounded">
 						<a className="mx-3 social-icon" href="https://facebook.com">{facebookIcon}</a>
 						<a className="mx-3 social-icon" href="https://twitter.com">{twitterIcon}</a>
+						<a className="mx-3 social-icon" href="https://instagram.com">{instagramIcon}</a>
 					</div>
 				</div>
 			</div>
