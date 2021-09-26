aa934431044832b008a58c0feaef7508b3f1500c
coders-sohan <coders.sohan.0216@gmail.com>
2021-09-26 14:24:23 +0600

    update cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Cart = (props) => {
-	const {cart} = props;
+const Cart = props => {
+	console.log(props);
+	const { cart } = props;
 	let total = 0;
 	for (const superHero of cart) {
 		total = total + superHero.salary;
@@ -10,9 +11,23 @@ const Cart = (props) => {
 		<div>
 			<h2>Total Added: {props.cart.length}</h2>
 			<h6 className="mt-4">Total Cost: {total}</h6>
+			<div className="my-4">
+				{
+					cart.map(person => <ShowName key={person.key} person={person}></ShowName>)
+				}
+			</div>
 			<button className="mt-4 btn btn-outline-info">Buy now</button>
 		</div>
 	);
 };
+
+const ShowName = props => {
+	console.log(props);
+	return(
+		<div className="card">
+			<p className="text-black">"<span className="fw-bold">Name:</span> {props.person.name}"</p>
+		</div>
+	);
+}
 
 export default Cart;
